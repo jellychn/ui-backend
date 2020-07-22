906aa370e24a8d6e05732ad5225c56ec1c162da6
Jerry <jchen123.862@gmail.com>
2020-07-23 00:43:56 +1200

    search query

--- app/controllers/items.controller.js
+++ app/controllers/items.controller.js
@@ -2,11 +2,28 @@ const db = require('../config/db');
 const { ObjectId } = require('mongodb');
 
 exports.getItems = async function(req, res) {
+    let combine = [{ gender:req.query.gender}]
+    if (Object.keys(req.query).includes('q')) {
+        const color = 'colors.' + req.query.q;
+        combine.push({$or: [{ name: new RegExp(`.*${req.query.q}.*`, "i") }, {[color]:{ $exists : true } }]});
+    }
+    if (req.query.category !== 'all') {
+        combine.push({ category: new RegExp(`.*${req.query.category.slice(0, -1)}.*`, "i") });
+    }
+
     db.connect().then((client) => {
-        client.db('ui').collection('items').find().toArray((err, result) => {
-            if (err) throw err
-            res.status(200).json(result);
-          })
+        if (Object.keys(req.query).length === 0) {
+            client.db('ui').collection('items').find({ "colors.black" : { $exists : true } }).toArray((err, result) => {
+                if (err) throw err
+                res.status(200).json(result);
+            })
+        } else {
+            client.db('ui').collection('items').find(
+                {$and: combine}).toArray((err, result) => {
+                if (err) throw err
+                res.status(200).json(result);
+            })            
+        }
     });
 };
 
