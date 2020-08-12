7f4cebe597076b02385c91cd0ec77ac6b3bd64d0
Jerry <jchen123.862@gmail.com>
2020-08-12 16:33:49 +1200

    update user infomation + password

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -72,14 +72,12 @@ exports.login = async function(req, res) {
         if (validated === 2) {
             db.connect().then(client => {
                 client.db('ui').collection('users').findOne({email:req.body.email}).then(result => {
-                    console.log(result)
                     if (result !== null) {
                         let hashedPassword = result.password;
-                        console.log(hashedPassword)
                         bcrypt.compare(req.body.password, hashedPassword).then((r) => {
                             if (r === true) {
                                 let token = jwt.encrypt(result._id.toString());
-                                client.db('ui').collection('users').update({email:req.body.email}, { $set: { 'token' : token  }})
+                                client.db('ui').collection('users').updateOne({email:req.body.email}, { $set: { 'token' : token  }})
                                 res.status(200).json({'token':token});
                             } else {
                                 res.status(200).json({'error':'email and password combination is incorrect'});
@@ -102,9 +100,12 @@ exports.login = async function(req, res) {
     }
 };
 
+exports.logout = async function(req, res) {
+    
+};
+
 exports.authenticated = async function(req, res) {
-    console.log(req.headers['x-authorization'])
-    if (req.headers['x-authorization'] !== null || req.headers['x-authorization'] !== '') {
+    if (req.headers['x-authorization'] !== null || req.headers['x-authorization'] !== '' || req.headers['x-authorization'] !== undefined) {
         db.connect().then(client => {
             client.db('ui').collection('users').findOne({token:req.headers['x-authorization']}).then(result => {
                 if (result !== null) {
@@ -121,10 +122,117 @@ exports.authenticated = async function(req, res) {
     }
 };
 
-exports.updateUserDetails = async function(req, res) {
+exports.getUser = async function(req, res) {
+    if (req.headers['x-authorization'] !== null || req.headers['x-authorization'] !== '' || req.headers['x-authorization'] !== undefined) {
+        db.connect().then(client => {
+            client.db('ui').collection('users').findOne({token:req.headers['x-authorization']}).then(result => {
+                if (result !== null) {
+                    res.status(200).json({
+                        'id_': result._id,
+                        'email': result.email,
+                        'firstname': result.firstname,
+                        'lastname': result.lastname
+                    });
+                } else {
+                    res.sendStatus(404);
+                }
+            }).catch(err => {
+                res.sendStatus(500);
+            });
+        }).catch(err => {
+            res.sendStatus(500);
+        });
+    } else {
+        res.sendStatus(404);
+    }
+};
 
+exports.updateUserDetails = async function(req, res) {
+    if (Object.keys(req.body).includes('email') && Object.keys(req.body).includes('firstname') && Object.keys(req.body).includes('lastname')) {
+        let validated = 0;
+        if (req.body.email.length > 0 && validateEmail(req.body.email)) {
+            validated += 1;
+        }
+        if (req.body.firstname.length > 0) {
+            validated += 1;
+        }
+        if (req.body.lastname.length > 0) {
+            validated += 1;
+        }
+    
+        if (validated === 3) {
+            if (req.headers['x-authorization'] !== null || req.headers['x-authorization'] !== '' || req.headers['x-authorization'] !== undefined) {
+                db.connect().then(client => {
+                    client.db('ui').collection('users').findOne({email:req.body.email}).then(result => {
+                        if (result !== null) {
+                            client.db('ui').collection('users').updateOne({token:req.headers['x-authorization']}, { $set: {'email':req.body.email, 'firstname':req.body.firstname, lastname:req.body.lastname}});
+                            res.sendStatus(200);
+                        } else {
+                            res.sendStatus(404);
+                        }
+                    }).catch(err => {
+                        res.sendStatus(404);
+                    });
+                }).catch(err => {
+                    res.sendStatus(500);
+                });
+            } else {
+                res.sendStatus(404);
+            }
+        } else {
+            res.sendStatus(500);
+        }
+    } else {
+        res.sendStatus(500);
+    }
 };
 
 exports.updateUserPassword = async function(req, res) {
-
+    if (Object.keys(req.body).includes('oldPassword') && Object.keys(req.body).includes('newPassword')) {
+        let validated = 0;
+        if (req.body.oldPassword.length > 0) {
+            validated += 1;
+        }
+        if (req.body.newPassword.length >= 8) {
+            validated += 1;
+        }
+    
+        if (validated === 2) {
+            if (req.headers['x-authorization'] !== null || req.headers['x-authorization'] !== '' || req.headers['x-authorization'] !== undefined) {
+                db.connect().then(client => {
+                    client.db('ui').collection('users').findOne({token:req.headers['x-authorization']}).then(result => {
+                        if (result !== null) {
+                            let hashedPassword = result.password;
+                            bcrypt.compare(req.body.oldPassword, hashedPassword).then((r) => {
+                                if (r === true) {
+                                    const saltRounds = 10;
+                                    let salt = bcrypt.genSaltSync(saltRounds);
+                                    let newHashedPassword = bcrypt.hashSync(req.body.newPassword, salt);
+                                    client.db('ui').collection('users').updateOne({token:req.headers['x-authorization']}, { $set: {'password':newHashedPassword}});
+                                    res.sendStatus(200);
+                                } else {
+                                    res.status(200).json({'error':'password does not match this account'});
+                                }
+                            });
+                        } else {
+                            res.sendStatus(404);
+                        }
+                    }).catch(err => {
+                        res.sendStatus(404);
+                    });
+                }).catch(err => {
+                    res.sendStatus(500);
+                    console.log('ss')
+                });
+            } else {
+                res.sendStatus(404);
+            }
+        } else {
+            res.sendStatus(500);
+            console.log('sss')
+        }
+    } else {
+        res.sendStatus(500);
+        console.log('ssss')
+    }
 };
--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -3,7 +3,9 @@ const users = require('../controllers/users.controller');
 module.exports = function(app) {
     app.route(app.rootUrl + '/users/register').post(users.register);
     app.route(app.rootUrl + '/users/login').post(users.login);
+    app.route(app.rootUrl + '/users/logout').post(users.logout);
     app.route(app.rootUrl + '/users/authenticated').post(users.authenticated);
+    app.route(app.rootUrl + '/users/user').post(users.getUser);
     app.route(app.rootUrl + '/users/updateDetails').post(users.updateUserDetails);
     app.route(app.rootUrl + '/users/updatePassword').post(users.updateUserPassword);
 };
